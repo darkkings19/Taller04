47c77afed465d5398af6c03ccc532d8683372d84
darkkings19 <p.paredes06@ufromail.cl>
2022-11-30 10:46:26 -0300

    final Commmint

--- Launcher/main.java
+++ Launcher/main.java
@@ -3,10 +3,11 @@ package Launcher;
 import GUI.Ventana_Inicial;
 import Modelo.DatosPais;
 import Modelo.Pais;
+import Modelo.TEAMS;
 
 public class main {
     public static void main(String[] args) {
-        Pais pais = new Pais();
+        TEAMS pais = new TEAMS();
         DatosPais.leerArchivoPaises(pais, "teams.txt");
         new Ventana_Inicial(pais);
     }
